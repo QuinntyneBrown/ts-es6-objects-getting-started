4b02ee8b5819299c30def8b0a306b56c9c3a94d0
QuinntyneBrown <quinntynebrown@gmail.com>
2016-04-14 17:08:47 -0400

    initial code

---
+++ wwwroot/main.js
@@ -0,0 +1,4 @@
+var foo = "foo";
+var bar = "bar";
+var fooBaz = { foo: foo, bar: bar };
+//# sourceMappingURL=main.js.map
---
+++ wwwroot/main.ts
@@ -0,0 +1,4 @@
+﻿var foo = "foo";
+var bar = "bar";
+
+var fooBaz = { foo, bar };
